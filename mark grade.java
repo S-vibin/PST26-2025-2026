0baf4c5df687239a6202e1db7a7c7987da0fa900
S-vibin <sr.vibinvibin2005@gmail.com>
2026-01-12 10:02:28 +0530

    Add files via upload

---
+++ mark grade.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main {
+  public static void main(String[] args) {
+    int mark = 76;
+    if(mark>=90) {
+        System.out.println("A grade");
+    }
+    else if(mark>=70) {
+        System.out.println("B grade");
+    }
+    else if(mark>=50) {
+        System.out.println("C grade");
+    }
+    else{    
+        System.out.println("Fail");
+    }
+  }
+}	
---
+++ week days.java
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main {
+  public static void main(String[] args) {
+    int num = 4;
+    if(num==1) {
+        System.out.println("monday");
+    }
+    else if(num==2) {
+        System.out.println("tuesday");
+    }
+    else if(num==3) {
+        System.out.println("wednesday");
+    }
+    else if(num==4) {
+        System.out.println("thursday");
+    
+    }
+    else if(num==5) {
+        System.out.println("friday");
+    }
+    else if(num==6) {
+        System.out.println("saturday");
+    }
+    else if(num==7) {
+        System.out.println("sunday");
+    }
+    else{    
+        System.out.println("there was no day from week");
+    }
+  }
+}	
