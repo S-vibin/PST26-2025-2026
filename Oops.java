e81fc0cef3b14257bce4ab5925e397b47650db5b
VIBIN  S <sr.vibinvibin2005@gmail.com>
2026-02-03 13:46:35 +0530

    Add files via upload

---
+++ Oops.java
@@ -0,0 +1,47 @@
+package PST;
+public class Oops {
+	public static void main(String[] args) {
+		Department sn=new Department();
+		sn.aids();
+		 method_demo();
+		
+		 
+	}
+	public static void method_demo() {
+		method_MikeTison();
+		System.out.print("hello");
+	}
+	public static void method_MikeTison() {
+		method_messi();
+		System.out.println("mike tison is the goat of boxer");
+	}
+	public static void method_messi() {
+		method_MikelJorden();
+		System.out.println("messi is the goat");
+	}
+	public static void method_MikelJorden() {
+		 
+		System.out.println("he is the goat of basketball");
+	   
+	}
+     
+}
+
+class Department {
+	public static void aids() {
+		System.out.println("members:"+"lal"+"--"+"bhai"+"--"+"arun"+"--"+"gopal");
+		cse();
+	}
+	public static void cse() {
+		method_it();
+		System.out.println("members:"+"chanduru "+"--"+"vijay");
+	}
+	public static void method_it() {
+		method_aiml();
+		System.out.println("members:"+"dinesh"+"--"+"danish"+"--"+"jeeva"+"--"+"saif");
+	}
+	public static void method_aiml() {
+		System.out.println("members:"+"amul"+"--"+"mahesh"+"--"+"darani"+"--"+"pradeep");
+	}
+	
+}
