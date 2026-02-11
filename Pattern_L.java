45189dc59574bc5181425b24bbc52e6c1b3c89fd
VIBIN  S <sr.vibinvibin2005@gmail.com>
2026-02-11 21:18:02 +0530

    Add files via upload

---
+++ Pattern_L.java
@@ -0,0 +1,17 @@
+package PST;
+import java.util.Scanner;
+public class Pattern_L {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter the number of star:");
+        int num=sc.nextInt();
+        for(int i=0;i<num;i++) {
+        	System.out.println("*");
+        	}for(int j=0;j<num;j++) {
+        		
+        		System.out.print("*"+" ");
+
+        }
+        }
+	}
